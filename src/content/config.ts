041aa1cc88a988187d56bc4822114d1d792655ff
Sarah Wilkes <aremnius@gmail.com>
2024-04-03 13:16:21 -0400

    update config w/ image schema so they can be passed as props in featured component

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,16 @@
-import { z, defineCollection } from 'astro:content';
+import type { SchemaContext } from "astro:content";
+import { z, defineCollection } from "astro:content";
+
+export const imageSchema = ({ image }: SchemaContext) =>
+  z.object({
+    src: image(),
+    alt: z.string(),
+  });
 
 const workCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
       title: z.string(),
       date: z.string(),
       liveURL: z.string().url(),
@@ -10,13 +18,10 @@ const workCollection = defineCollection({
       abstract: z.string(),
       builtWith: z.string(),
       featured: z.boolean(),
-      image: z.object({
-        src: z.string(),
-        alt: z.string(),
-      }),
+      image: imageSchema({ image }),
     }),
 });
 
 export const collections = {
-    'work': workCollection,
+  work: workCollection,
 };

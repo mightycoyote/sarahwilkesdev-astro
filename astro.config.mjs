cee9aba5691c97e4a2057e40c94cf960e05c1f84
Sarah Wilkes <aremnius@gmail.com>
2024-04-04 13:35:20 -0400

    fix display of some Prism blocks, add webmanifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+import webmanifest from 'astro-webmanifest';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    webmanifest({
+      name: 'Sarah Wilkes Dev',
+      description: 'Frontend web developer portfolio',
+      siteUrl: 'https://www.sarahwilkesdev.com',
+      image: '/Sarah_octocat_social.jpg',
+      icon: 'src/images/favicon.svg',
+      background_color: '#0a0a0a',
+      theme_color: 'rgb(82, 99, 159)',
+      start_url: '/',
+      display: 'standalone',
+    }),
+  ],
   markdown: {
     syntaxHighlight: 'prism',
   }
